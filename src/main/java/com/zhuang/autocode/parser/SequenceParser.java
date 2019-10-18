54507f59b22578f5e7eb43a241e86e802f044a6a
zwb <zhuangweibin@stbl.cc>
2019-10-18 14:02:05 +0800

    aa

--- src/main/java/com/zhuang/autocode/parser/SequenceParser.java
+++ src/main/java/com/zhuang/autocode/parser/SequenceParser.java
@@ -8,72 +8,55 @@ import com.zhuang.autocode.model.AutoCodeDetail;
 
 public class SequenceParser implements Parser {
 
-	public String getName() {
-		return "seq";
-	}
+    public String getName() {
+        return "seq";
+    }
 
-	public String parse(ParserContext context) {
-		String result = "";
-		result = getSeq(context);
-		return result;
-	}
+    public String parse(ParserContext context) {
+        String result = "";
+        result = getSeq(context);
+        return result;
+    }
 
-	private String getSeq(ParserContext context) {
-		String[] arParam = context.getParameter().split(",");
-		int minLength = 0;
-		boolean keepIncrease = false;
-		Integer intSeq = 0;
-
-		minLength = Integer.parseInt(arParam[0]);
-
-		if (arParam.length >= 2) {
-			if (arParam[1] != "0") {
-				keepIncrease = true;
-			}
-		}
-
-		String prefixCode = context.getParsedText().split("\\{")[0];
-
-		if (keepIncrease) {
-			prefixCode = "none";
-		}
-
-		AutoCodeDetail detailModel = context.getService()
-				.getDetailByPrefixCode(context.getAutoCode().getId(), prefixCode);
-
-		Date dtNow = new Date();
-
-		if (detailModel != null) {
-			detailModel.setSeq(detailModel.getSeq() + 1);
-			detailModel.setModifiedTime(dtNow);
-
-			context.getService().saveDetail(detailModel);
-		} else {
-			detailModel = new AutoCodeDetail();
-			detailModel.setId(UUID.randomUUID().toString());
-			detailModel.setAutocodeId(context.getAutoCode().getId());
-			detailModel.setPrefixCode(prefixCode);
-			detailModel.setSeq(1);
-			detailModel.setCreatedTime(dtNow);
-			detailModel.setModifiedTime(dtNow);
-
-			context.getService().addDetail(detailModel);
-		}
-
-		int finalLength = (detailModel.getSeq().toString().length() > minLength
-				? detailModel.getSeq().toString().length() : minLength);
-
-		String dfPattern = "";
-
-		for (int i = 0; i < finalLength; i++) {
-			dfPattern = dfPattern + "0";
-		}
-
-		intSeq = detailModel.getSeq();
-
-		DecimalFormat df = new DecimalFormat(dfPattern);
-
-		return df.format(intSeq);
-	}
+    private String getSeq(ParserContext context) {
+        String[] arParam = context.getParameter().split(",");
+        int minLength = 0;
+        boolean keepIncrease = false;
+        Integer intSeq = 0;
+        minLength = Integer.parseInt(arParam[0]);
+        if (arParam.length >= 2) {
+            if (arParam[1] != "0") {
+                keepIncrease = true;
+            }
+        }
+        String prefixCode = context.getParsedText().split("\\{")[0];
+        if (keepIncrease) {
+            prefixCode = "none";
+        }
+        AutoCodeDetail detailModel = context.getService().getDetailByPrefixCode(context.getAutoCode().getId(), prefixCode);
+        Date dtNow = new Date();
+        if (detailModel != null) {
+            detailModel.setSeq(detailModel.getSeq() + 1);
+            detailModel.setModifiedTime(dtNow);
+            context.getService().saveDetail(detailModel);
+        } else {
+            detailModel = new AutoCodeDetail();
+            detailModel.setId(UUID.randomUUID().toString());
+            detailModel.setAutocodeId(context.getAutoCode().getId());
+            detailModel.setPrefixCode(prefixCode);
+            detailModel.setSeq(1);
+            detailModel.setCreatedTime(dtNow);
+            detailModel.setModifiedTime(dtNow);
+            context.getService().addDetail(detailModel);
+        }
+        int finalLength = (detailModel.getSeq().toString().length() > minLength ? detailModel.getSeq().toString().length() : minLength);
+        String dfPattern = "";
+        for (int i = 0; i < finalLength; i++) {
+            dfPattern = dfPattern + "0";
+        }
+        intSeq = detailModel.getSeq();
+        DecimalFormat df = new DecimalFormat(dfPattern);
+        return df.format(intSeq);
+    }
 
 }
